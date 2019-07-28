c59afbdc3aca019ae585b609579fe698bfc3c4a9
Khinshan Khan <khanshan501@gmail.com>
2019-07-28 13:24:37 -0400

    de/elfeed-search-filters implemented

--- dashboard-elfeed.el
+++ dashboard-elfeed.el
@@ -5,7 +5,7 @@
 ;; Author: Khinshan Khan <khinshan.khan@gmail.com>
 ;; URL: https://github.com/kkhan01/emacs-dashboard-elfeed
 ;; Version: 0.0.1
-;; Package-Requires: ((emacs "24") (dashboard "1.2.5") (elfeed "3.1.0"))
+;; Package-Requires: ((emacs "24") (dashboard) (elfeed))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -24,10 +24,41 @@
 
 ;; Display a filtered search from Elfeed in Dashboard widget.
 
+;; See README.org for installation and usage.
+
 ;;; Code:
 
 (require 'elfeed)
 (require 'dashboard)
+
+;;;###autoload
+(defun de/elfeed-search-filters (&optional search-filter-arg)
+  "Wrapper for better searching. Will prompt user for filter terms, or else use given argument. Can be use in a hook too."
+  (interactive)
+  (unless search-filter-arg
+    (setq search-filter-arg
+          ;; ask for user input if need be
+          (split-string (read-string "Enter your filter terms:") split-string-default-separators)))
+  ;; this is a slightly hacky solution to determine if time is provided
+  (setq timewords '("-day-ago" "-days-ago" "-month-ago" "-months-ago" "-year-ago" "-years-ago"))
+  (setq search-filter-arg (string-join
+                           ;; normalize the search terms for elfeed filter function
+                           (mapcar (apply-partially (lambda (arg)
+                                                      (cond ((or (string-prefix-p "+" arg)
+                                                                 (string-prefix-p "-" arg)
+                                                                 (string-prefix-p "@" arg)
+                                                                 (string-prefix-p "#" arg)) arg)
+                                                            ;; determine if term is a time term
+                                                            ((member 't
+                                                                     (mapcar (apply-partially (lambda (timeword)
+                                                                                                (string-suffix-p timeword arg))) timewords))
+                                                             (concat "@" arg))
+                                                            ;; heuristic that everything not explicitly excluded is included
+                                                            (t (concat "+" arg)))
+                                                      ))
+                                   search-filter-arg)
+                           " "))
+  (elfeed-search-set-filter search-filter-arg))
 
 (provide 'dashboard-elfeed)
 ;;; dashboard-elfeed.el ends here
