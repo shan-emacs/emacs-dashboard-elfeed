73bbe0e4bbeba7d3892803aeaac2d3d7a1ee9f54
Khinshan Khan <khanshan501@gmail.com>
2019-07-29 22:29:35 -0400

    contemplating giving user access to widget title as well

--- dashboard-elfeed.el
+++ dashboard-elfeed.el
@@ -126,66 +126,84 @@ Filter is determined by RES (which user shouldn't interact with)."
       (elfeed-search-show-entry de/entry))
     (switch-to-buffer buffname)))
 
-(defun dashboard-elfeed-template (list-size key search-filter res)
-  "Add the template dashboard widgets.
-ARGS: KEY LIST-SIZE SEARCH-FILTER RES."
-  (dashboard-insert-section
-   (concat "Elfeed: [" search-filter "]")
-   ;; list generated for dashboard
-   (de/elfeed-list list-size search-filter res)
-   list-size
-   key
-   ;; decide what to do when user clicks on item
-   `(lambda (&rest ignore)
-      (de/elfeed-list-interact ',el res))
-   ;; displays list in dashboard
-   (format "%s" el)))
-
 (defun dashboard-elfeed (list-size)
   "Add the elfeed functionality to dashboard.
 Makes the list as long as LIST-SIZE."
-  (dashboard-elfeed-template list-size
-                             de/key
-                             de/dashboard-search-filter
-                             de/dashboard-results))
+  (dashboard-insert-section
+   (concat "Elfeed: [" de/dashboard-search-filter "]")
+   ;; list generated for dashboard
+   (de/elfeed-list list-size de/dashboard-search-filter de/dashboard-results)
+   list-size
+   de/key
+   ;; decide what to do when user clicks on item
+   `(lambda (&rest ignore)
+      (de/elfeed-list-interact ',el de/dashboard-results))
+   ;; displays list in dashboard
+   (format "%s" el)))
 
 (defun dashboard-elfeed-1 (list-size)
   "Refer to dashboard-elfeed.
 For the sake of having multiple filters.
 ARGS: LIST-SIZE."
-  (dashboard-elfeed-template list-size
-                             de/key-1
-                             de/dashboard-search-filter-1
-                             de/dashboard-results-1))
+  (dashboard-insert-section
+   (concat "Elfeed: [" de/dashboard-search-filter-1 "]")
+   ;; list generated for dashboard
+   (de/elfeed-list list-size de/dashboard-search-filter-1 de/dashboard-results-1)
+   list-size
+   de/key-1
+   ;; decide what to do when user clicks on item
+   `(lambda (&rest ignore)
+      (de/elfeed-list-interact ',el de/dashboard-results-1))
+   ;; displays list in dashboard
+   (format "%s" el)))
 
 (defun dashboard-elfeed-2 (list-size)
   "Refer to dashboard-elfeed.
 For the sake of having multiple filters.
 ARGS: LIST-SIZE."
-  (dashboard-elfeed-template list-size
-                             de/key-2
-                             de/dashboard-search-filter-2
-                             de/dashboard-results-2))
+  (dashboard-insert-section
+   (concat "Elfeed: [" de/dashboard-search-filter-2 "]")
+   ;; list generated for dashboard
+   (de/elfeed-list list-size de/dashboard-search-filter-2 de/dashboard-results-2)
+   list-size
+   de/key-2
+   ;; decide what to do when user clicks on item
+   `(lambda (&rest ignore)
+      (de/elfeed-list-interact ',el de/dashboard-results-2))
+   ;; displays list in dashboard
+   (format "%s" el)))
 
 (defun dashboard-elfeed-3 (list-size)
   "Refer to dashboard-elfeed.
 For the sake of having multiple filters.
 ARGS: LIST-SIZE."
-  (dashboard-elfeed-template list-size
-                             de/key-3
-                             de/dashboard-search-filter-3
-                             de/dashboard-results-3))
+  (dashboard-insert-section
+   (concat "Elfeed: [" de/dashboard-search-filter-3 "]")
+   ;; list generated for dashboard
+   (de/elfeed-list list-size de/dashboard-search-filter-3 de/dashboard-results-3)
+   list-size
+   de/key-3
+   ;; decide what to do when user clicks on item
+   `(lambda (&rest ignore)
+      (de/elfeed-list-interact ',el de/dashboard-results-3))
+   ;; displays list in dashboard
+   (format "%s" el)))
 
 (defun dashboard-elfeed-4 (list-size)
   "Refer to dashboard-elfeed.
 For the sake of having multiple filters.
 ARGS: LIST-SIZE."
-  (dashboard-elfeed-template list-size
-                             de/key-4
-                             de/dashboard-search-filter-4
-                             de/dashboard-results-4))
-
-
+  (dashboard-insert-section
+   (concat "Elfeed: [" de/dashboard-search-filter-4 "]")
+   ;; list generated for dashboard
+   (de/elfeed-list list-size de/dashboard-search-filter-4 de/dashboard-results-4)
+   list-size
+   de/key-4
+   ;; decide what to do when user clicks on item
+   `(lambda (&rest ignore)
+      (de/elfeed-list-interact ',el de/dashboard-results-4))
+   ;; displays list in dashboard
+   (format "%s" el)))
 
 (provide 'dashboard-elfeed)
 ;;; dashboard-elfeed.el ends here
